13d95f4692878a13c5e4bc1cea6d29ce94b4f483
Dan Larsen <dlarsen023@yahoo.com>
2021-10-27 22:53:43 -0700

    cleaned up html

---
+++ stuff/js/main.js
@@ -0,0 +1,39 @@
+const { ipcRenderer } = require("electron")
+const makeEngine = require("./src/engine.js")
+
+const getCursorPosition = (canvas, event) => {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    return {x, y}
+}
+const logMsg = msg => {
+    var $log = $("#log")
+    $log.append(msg)
+    $log[0].scrollTop = $log[0].scrollHeight;
+}
+
+window.onerror = function(msg, url, line, col, error) {
+    var extra = !col ? '' : '\ncolumn: ' + col;
+    extra += !error ? '' : '\nerror: ' + error;
+
+    var suppressErrorAlert = false;
+    ipcRenderer.invoke("showDev")
+    return suppressErrorAlert;
+}
+
+const engine = makeEngine(msg => logMsg)
+
+$(() => {
+    engine.init((step, msg) => {
+        $("#log").append(`Step ${step} done: ${msg}.<br />`)
+    })
+    engine.render()
+
+    $("canvas").on('mousedown', e => {
+        var coords = getCursorPosition(e.target, e)
+        coords.x = Math.trunc(coords.x /= 20)
+        coords.y = Math.trunc(coords.y /= 20)
+        logMsg(engine.renderer.getTileAt(coords.x, coords.y).desc + "<br />")
+    })
+})
