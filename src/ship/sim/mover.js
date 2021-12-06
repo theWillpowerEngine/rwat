1da85868fa20dcc6fba411f875505279432b436f
Dan Larsen <dlarsen023@yahoo.com>
2021-12-05 21:24:07 -0800

    refactor lift vs. gravity

--- src/ship/sim/mover.js
+++ src/ship/sim/mover.js
@@ -5,20 +5,28 @@ module.exports = (eng) => {
     let world = engine.world
     let vector = ship.movementVector
 
-    //gravity
-    let targetFall = ship.weight / 2.369
-    targetFall = targetFall * -1
-    if(vector.z > targetFall) {
-        vector.z -= eng.conf.forceOfGravity
-        if(vector.z < targetFall)
-            vector.z = targetFall 
+    //gravity + lift
+    let targetFall = -1 * (ship.weight / 2.369)
+    let targetLift = (lift.temperature - 11) * 1.239
+    if(targetLift < 0)  targetLift = 0
+    let deltaZ = targetLift - targetFall
+
+    //going down?
+    if(deltaZ < vector.z) {
+        if(deltaZ < eng.conf.forceOfGravity)
+            vector.z = deltaZ
+        else
+            vector.z -= eng.conf.forceOfGravity 
+    
+    //going up
+    } else if(deltaZ < 0) {
+        let maxLiftTolerance = (25 - lift.getDamage() / 5)
+        if(deltaZ > maxLiftTolerance) {
+            lift.damage(Math.round(deltaZ - maxLiftTolerance))
+            deltaZ = deltaZ / 10
+        }
+        vector.z += deltaZ
     }
 
-    //lift
-    let targetLift = (lift.temperature - 11) * 1.239
-    if(targetLift < 0)
-        targetLift = 0
-    vector.z += targetLift
-
-    //TODO:  apply movement and limit based on tie
+    //TODO:  apply movement, limit based on tie, check for crash landing and gridsplash
 }
