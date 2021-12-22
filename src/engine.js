40d64bbf2970726e867687ee313826af8802dbb7
Dan Larsen <dlarsen023@yahoo.com>
2021-12-21 19:45:03 -0800

    bullies CG - incremental

--- src/engine.js
+++ src/engine.js
@@ -186,7 +186,7 @@ module.exports = (logger, opts) => {
                         var cmd = pop()
                         switch(cmd) {
                             case "start":
-                                cgPicks.push(rpg.pickOne(cgAll))
+                                cgPicks.push("cg1")  //rpg.pickOne(cgAll))      TODO
                                 cgPicks.push(rpg.pickOne(cgAll))
                                 cgPicks.push(rpg.pickOne(cgAll))
                                 break
