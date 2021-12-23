85730090dfefb3e29e9a4b58811d8acd9b613a82
Dan Larsen <dlarsen023@yahoo.com>
2021-12-22 21:20:39 -0800

    Added some names

--- src/world/gens/crewNameGen.js
+++ src/world/gens/crewNameGen.js
@@ -1,15 +1,19 @@
 const rpg = require("../../system.js")
 
 const firstNames = [
-    "Alex", "Alvin", "Anomander", "Benedict", "Blaise", "Bob", "Brand", "Brandon", "Corwin", "David", "Delwyn", "Dworkin", 
-    "Gerard", "Ian", "Iain", "Jack", "Jams", "Luke", "Mat", "Maddox", "Matt", "Oberon", "Perrin", "Philodendron", "Rand", 
-    "Random", "Robert", "Stephen", "Steven", "Terry",  "Terrence", "Tom", "Tomas", "Wyndham"
+    "Alex", "Alvin", "Anomander", "Arnold", "Baggy", "Benedict", "Blaise", "Bob", "Brand", "Brandon", "Connor", "Corwin", 
+    "Dan", "Daniel", "David", "Delwyn", "Dolph", "Duncan", "Dworkin", "Gerard", "Hank", "Harrison", "Harry", "Henry", "Ian", 
+    "Iain", "Jack", "James", "Kevin", "Leland", "Luke", "Mat", "Maddox", "Matt", "Oberon", "Perrin", "Phillip", 
+    "Philodendron", "Rand", "Random", "Rob", "Robert", "Sam", "Samuel", "Shep", "Shen", "Stephen", "Steven", "Terry", 
+    "Terrence", "Tom", "Tomas", "Wyndham"
 ]
 
 const lastNames = [
-    "Abnett", "Aubrey", "Babbington", "Banks", "Barimen", "Bennett", "Case", "Damodred", "Goodkind", "Holmes", "Jordan", 
-    "Lynch", "Maturin", "Mishima", "Moappit", "Montgomery", "Patel", "Pullings", "Rake", "Reynold", "Roddenberry", 
-    "Sanderson", "Senjak", "Trakand", "Verne", "Wells"
+    "Abnett", "Adler", "Amnell", "Aubrey", "Babbington", "Banks", "Barimen", "Bennett", "Black", "Butler", "Case", "Clavell", 
+    "Cooper", "Damodred", "Dumas", "Goodkind", "Hayward", "Haywood", "Heyward", "Holmes", "Jordan", "Li", "Lien", "Lovecraft", 
+    "Lynch", "Martell", "Maturin", "Meredith", "Mishima", "Moappit", "Montgomery", "Moriarty", "Mulder", "Mulgrew", 
+    "Palmer", "Park", "Patel", "Picard", "Picardo", "Pullings", "Rake", "Ravenscroft", "Rearden", "Reynold", "Roddenberry", 
+    "Sanderson", "Senjak", "Stevens", "Stevenson", "Taggart", "van Damme", "van Diem", "Trakand", "Verne", "Wells", "Woods"
 ]
 
 module.exports = {
